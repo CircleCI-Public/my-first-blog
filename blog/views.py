f18a9bccfdab6ed2f019cb4a77919b97a0d86dd1
Benish Khan <benish@circleci.com>
2019-10-30 16:28:49 -0700

    Add all updates to views and resolve user object error

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -27,7 +27,6 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
